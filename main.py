1e8d76c9cb733b077adc7fd89d57ac47f427ca3b
MinhN62204 <128069854+MinhN62204@users.noreply.github.com>
2023-03-16 12:52:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import random
 
+def rollDice(sides):
+  result = random.randint(1,sides)
+  return result
+
+def health():
+  healthStat = ((rollDice(6)*rollDice(12))/2)+10
+  return healthStat
+
+def strength():
+  strengthStat = ((rollDice(6)*rollDice(8))/2)+12
+  return strengthStat
+
+while True:
+  print("⚔️  CHARACTER BUILDER ⚔️")
+  print()
+  name = input("Name your Legend:\n")
+  type = input("Character Type (Human, Elf, Wizard, Orc):\n")
+  print()
+  print(name, "|", type)
+  print("HEALTH:", health())
+  print("STRENGTH:", strength())
+  print()
